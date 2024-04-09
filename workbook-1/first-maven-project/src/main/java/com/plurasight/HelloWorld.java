7662a982e79b7b1fc39221333e62128c4d80badb
Onriel Houston <onrielhouston@gmail.com>
2024-04-09 11:06:49 -0400

    completed shopping list add

---
+++ workbook-1/first-maven-project/src/main/java/com/plurasight/HelloWorld.java
@@ -0,0 +1,8 @@
+package com.plurasight;
+
+public class HelloWorld {
+    //This is the Main method- entry point is the JVM new*
+    public static void main(String[]args){
+        System.out.println("Hello World");
+    }
+}
