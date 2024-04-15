92df88f75696264fe17e0058e81d17d6cfe93015
Onriel Houston <onrielhouston@gmail.com>
2024-04-15 11:09:24 -0400

    completed workbook 2 assignment

---
+++ workbook-2/FullNameGenerator/src/main/java/com/plurasight/FullNameApplication.java
@@ -0,0 +1,30 @@
+package com.plurasight;
+
+import java.util.Scanner;
+
+public class FullNameApplication {
+    public static void main(String[]args) {
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("Please Enter Your name!");
+        System.out.print("First Name:");
+        String firstName = scanner.nextLine();
+        firstName = firstName.trim();
+        System.out.print("Middle Name:");
+        String middleName = scanner.nextLine();
+        middleName = middleName.trim();
+        System.out.print("Last Name:");
+        String lastName = scanner.nextLine();
+        lastName = lastName.trim();
+        System.out.print("Suffix:");
+        String suffix = scanner.nextLine();
+        suffix=suffix.trim();
+        String fullName = (firstName + " " + middleName + " " + lastName + " " +suffix);
+        System.out.println(fullName);
+
+
+
+
+
+
+    }
+}
