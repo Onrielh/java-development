431184a017ed9de0d1507fb2d668754c182b3369
Onriel Houston <onrielhouston@gmail.com>
2024-04-18 11:21:16 -0400

    completed excercise

---
+++ workbook-2/LoopingExercises/src/main/java/com/plurasight/DoWhileLoop.java
@@ -0,0 +1,13 @@
+package com.plurasight;
+
+public class DoWhileLoop {
+    public static void main(String[]args){
+        int count =1; // beggining condition
+        do{
+            System.out.println("I Love Java"); // do part
+            count ++;} // increment
+        while (count<=5); // ending codition/ while part
+
+
+    }
+}
---
+++ workbook-2/LoopingExercises/src/main/java/com/plurasight/WhileLoop.java
@@ -0,0 +1,11 @@
+package com.plurasight;
+
+public class WhileLoop {
+    public static void main(String[]args){
+       int count =1; // beggining condition
+       while( count <= 5){ // ending condition
+           System.out.println("I Love Java");
+           count ++; // increment
+       }
+    }
+}
